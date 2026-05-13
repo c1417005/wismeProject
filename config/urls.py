abe97a0119b1f5857fd5168efa964986b60ce2a4
soufu <c1417005@st.kanazawa-it.ac.jp>
2026-05-13 18:35:02 +0900

    uelの設定

--- config/urls.py
+++ config/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/wisme/', permanent=False)),
     path('admin/', admin.site.urls),
     path('i18n/', include('django.conf.urls.i18n')),
     path('accounts/', include('allauth.urls')),
